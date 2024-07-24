9d9e52c5b6cdb26311ba76e26fbf469763c3ad55
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2024-07-24 17:20:15 +0100

    Landing Page done!

--- IdeaAvengers/frontend/src/App.jsx
+++ IdeaAvengers/frontend/src/App.jsx
@@ -8,16 +8,7 @@ function App() {
         <div className="logo">
           <img src="./images/logo.png" alt="Avocata Logo" />
         </div>
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Team</a></li><br></br>
-          </ul>
-        </nav>
-        <div className="auth-buttons">
-          <button className="log-in">LOG IN</button>
-        </div>
+        
       </header>
       <main>
         <div className="hero">
@@ -25,14 +16,29 @@ function App() {
             <img src="./images/avocado-chef.png" alt="Avocado Chef" />
           </div>
           <div className="hero-text">
-            <h1>We make eating healthy fun.<br></br>for you.</h1>
-            <p>AI-powered nutrition calculator.<br></br>
+            <h1>We make eating healthy fun<br></br>for you.</h1>
+            <p>AI-powered nutrition calculator<br></br>
               Recipe builder.<br></br>
-              Personalized cooking classes.</p>
+              Personalized cooking classes</p>
             <button className="get-started">Get Started</button>
           </div>
         </div>
       </main>
+
+      <div className="text-information">
+        <div className="column left">
+          <h2>Recipe Builder</h2>
+          <p>Elevate your culinary experience with the Recipe Builder! Create custom recipes tailored to your dietary preferences and nutritional goals, with detailed nutritional information and easy-to-follow instructions.</p>
+        </div>
+        <div className="column center">
+          <h2>Nutrition Calculator</h2>
+          <p>Track your daily intake and plan balanced meals with the Nutrition Calculator! Tailored to your health goals, it provides detailed nutritional information for thousands of foods.</p>
+        </div>
+        <div className="column right">
+          <h2>Cooking Classes</h2>
+          <p>Transform you kitchen skills with our gamified cooking classes! Enjoy hands-on, interactive lessons in different cuisines from low to high level designed to fit your culinary goals.</p>
+        </div>
+      </div>
     </>
   );
 }
