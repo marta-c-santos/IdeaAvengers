a4fbc22c223ff891f0de26cae225b579d5778f43
Marta Santos <60897114+marta-c-santos@users.noreply.github.com>
2024-07-24 17:28:10 +0100

    Updated

--- IdeaAvengers/frontend/src/App.jsx
+++ IdeaAvengers/frontend/src/App.jsx
@@ -17,10 +17,10 @@ function App() {
           </div>
           <div className="hero-text">
             <h1>We make eating healthy fun<br></br>for you.</h1>
-            <p>AI-powered nutrition calculator<br></br>
-              Recipe builder.<br></br>
-              Personalized cooking classes</p>
-            <button className="get-started">Get Started</button>
+            <p>Personalized cooking classes<br></br>
+              Recipe builder<br></br>
+              AI-powered nutrition calculator</p>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSe5jahsJzJ6SPiZ-1iswju48UwF_1L_5z_egq20I9wXnup-Kg/viewform?usp=sf_link" className="get-started">Get Started</a>
           </div>
         </div>
       </main>
@@ -31,12 +31,12 @@ function App() {
           <p>Elevate your culinary experience with the Recipe Builder! Create custom recipes tailored to your dietary preferences and nutritional goals, with detailed nutritional information and easy-to-follow instructions.</p>
         </div>
         <div className="column center">
-          <h2>Nutrition Calculator</h2>
-          <p>Track your daily intake and plan balanced meals with the Nutrition Calculator! Tailored to your health goals, it provides detailed nutritional information for thousands of foods.</p>
-        </div>
-        <div className="column right">
           <h2>Cooking Classes</h2>
           <p>Transform you kitchen skills with our gamified cooking classes! Enjoy hands-on, interactive lessons in different cuisines from low to high level designed to fit your culinary goals.</p>
+        </div>
+        <div className="column right">
+          <h2>Nutrition Calculator</h2>
+          <p>Track your daily intake and plan balanced meals with the Nutrition Calculator! Tailored to your health goals, it provides detailed nutritional information for thousands of foods.</p>
         </div>
       </div>
     </>
